45cc58c2071a6b641c0cd3b92f84f8d0d7b17301
Ronaldo Nunes <projetoftnunes@gmail.com>
2023-02-18 12:28:20 -0300

    feat: estruturação de pastas

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-
+// Inluir arquivos nas pastas mesmo que vazias para incluí-las no versionamento de código
+// Fazer commits pequenos
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
