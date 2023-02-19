2e05431b238df86ba1d1e1e3f60bda2f64a1b095
Ronaldo Nunes <projetoftnunes@gmail.com>
2023-02-19 11:42:56 -0300

    feat: atualização do,layou básico de página

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,8 +3,8 @@ import { LayoutBaseDePagina } from '../../shared/layouts';
 export const Dashboard = () => {
 
   return (
-    <LayoutBaseDePagina titulo='Página inicial'>
-
+    <LayoutBaseDePagina titulo='Página inicial' barraDeFerramentas={<>Barra de ferramentas</>}>
+      Teste
     </LayoutBaseDePagina>
   );
 };
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -5,31 +5,38 @@ import { useDrawerContext } from '../contexts';
 
 interface ILayoutBaseDePaginaProps {
   titulo: string;
+  barraDeFerramentas?: ReactNode;
   children: ReactNode;
 }
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children, titulo }: ILayoutBaseDePaginaProps) => {
-  const theme = useTheme();
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ titulo, children, barraDeFerramentas }: ILayoutBaseDePaginaProps) => {
   const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const theme = useTheme();
 
   const { toggleDrawerOpen } = useDrawerContext(); // Hook personalizado que fornece o contexto especificado
 
   return (
     <Box height='100%' display='flex' flexDirection='column' gap={1}>
 
-      <Box display='flex' alignItems='center' padding={1} height={theme.spacing(8)} gap={1}>
+      <Box display='flex' alignItems='center' gap={1} padding={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} >
         {smDown && <IconButton onClick={toggleDrawerOpen}>
           <Icon>menu</Icon>
         </IconButton>}
-        <Typography variant='h5'>
+        <Typography
+          whiteSpace='nowrap'
+          overflow='hidden'
+          textOverflow='ellipsis'
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+        > 
           {titulo}
         </Typography>
       </Box>
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {barraDeFerramentas && <Box >
+        {barraDeFerramentas}
+      </Box>}
 
-      <Box>
+      <Box flex={1} overflow='auto'> {/**O scroll da página ficará retido a este box (overflow=auto), que receberá os layouts diversos da aplicação */}
         {children}
       </Box>
     </Box>
